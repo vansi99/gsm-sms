815f0d3872f05df88b48f6c7a572b0989dda83b6
vansi99 <vansido99@gmail.com>
2019-06-25 09:49:47 +0700

    change

--- test/sms.js
+++ test/sms.js
@@ -7,7 +7,7 @@ const NATS_PORT = "4222";
 
 let pcLocal = postman.createClient({'url': `nats://${NATS_HOST}:${NATS_PORT}`}, namespace.local);
 const data = {
-    message: "tét",
+    message: "ttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttrtttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt",
     to: "+84966823635"
 };
 
